67cebe001a33e530c40048f310fdec98cab4245a
fchavonet <fchavonet@mailo.com>
2023-12-20 16:58:35 +0100

    First upload of task "5. Some stats?".

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,21 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
     """Endpoint to retrieve the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    return {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
