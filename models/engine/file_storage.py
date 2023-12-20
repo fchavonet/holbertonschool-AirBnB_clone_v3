e0f8ee72887856caa759b200ef580d0cc36ba0a3
fchavonet <fchavonet@mailo.com>
2023-12-20 11:53:27 +0100

    Add "get" and "count" method to engines.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieves one object"""
+        retrieved_objects = self.all(cls)
+        for object in retrieved_objects.values():
+            if id == object.id:
+                return object
+        return None
+
+    def count(self, cls=None):
+        """Returns number of objects in storage matching a given class"""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
